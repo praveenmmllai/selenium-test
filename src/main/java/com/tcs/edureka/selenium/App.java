99ae620d0cee3499a25a4d49b18c6dd6f5546b86
Praveen Pillai <praveenmmllai@gmail.com>
2021-06-15 13:36:59 +0530

    Removed fat jar and added test case

--- src/main/java/com/tcs/edureka/selenium/App.java
+++ src/main/java/com/tcs/edureka/selenium/App.java
@@ -12,32 +12,32 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class App {
 	@Test
 	public static void main(String[] args) throws InterruptedException {
-
-		System.setProperty("webdriver.chrome.driver", "C:\\Softwares\\Selenium\\chromedriver_win32\\chromedriver.exe");
-		ChromeOptions chromeOptions = new ChromeOptions();
-		WebDriver driver = new ChromeDriver(chromeOptions);
-
-		System.out.println("Hi, Welcome to testing Edureka bootcamp sample website");
-
-		driver.get("http://192.168.99.100:3001/");
-
-		WebElement username = driver.findElement(By.name("login"));
-		WebElement password = driver.findElement(By.name("password"));
-		WebElement login = driver.findElement(By.name("click"));
-
-		username.sendKeys("devops");
-		password.sendKeys("test");
-		login.click();
-
-		String loginsucceeded = driver.findElement(By.cssSelector(".alert-link")).getText();
-		
-		Assert.assertEquals(loginsucceeded, "Login Succeeded");
-		
-		System.out.println("Test successful");
-		Thread.sleep(10000);
-		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-
-		driver.quit();
+		/*
+		 * System.setProperty("webdriver.chrome.driver",
+		 * "C:\\Softwares\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		 * ChromeOptions chromeOptions = new ChromeOptions(); WebDriver driver = new
+		 * ChromeDriver(chromeOptions);
+		 * 
+		 * System.out.println("Hi, Welcome to testing Edureka bootcamp sample website");
+		 * 
+		 * driver.get("http://192.168.99.100:3001/");
+		 * 
+		 * WebElement username = driver.findElement(By.name("login")); WebElement
+		 * password = driver.findElement(By.name("password")); WebElement login =
+		 * driver.findElement(By.name("click"));
+		 * 
+		 * username.sendKeys("devops"); password.sendKeys("test"); login.click();
+		 * 
+		 * String loginsucceeded =
+		 * driver.findElement(By.cssSelector(".alert-link")).getText();
+		 * 
+		 * Assert.assertEquals(loginsucceeded, "Login Succeeded");
+		 * 
+		 * System.out.println("Test successful"); Thread.sleep(10000);
+		 * driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		 * 
+		 * driver.quit();
+		 */
 	}
 
 }
--- src/test/java/com/tcs/edureka/selenium/AppTest.java
+++ src/test/java/com/tcs/edureka/selenium/AppTest.java
@@ -1,38 +1,50 @@
 package com.tcs.edureka.selenium;
 
-import junit.framework.Test;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	@Test
+	public void testSelenium() throws InterruptedException {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+		System.setProperty("webdriver.chrome.driver", "C:\\Softwares\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		ChromeOptions chromeOptions = new ChromeOptions();
+		WebDriver driver = new ChromeDriver(chromeOptions);
+
+		System.out.println("Hi, Welcome to testing Edureka bootcamp sample website");
+
+		driver.get("http://192.168.99.100:3001/");
+
+		WebElement username = driver.findElement(By.name("login"));
+		WebElement password = driver.findElement(By.name("password"));
+		WebElement login = driver.findElement(By.name("click"));
+
+		username.sendKeys("devops");
+		password.sendKeys("test");
+		login.click();
+
+		String loginsucceeded = driver.findElement(By.cssSelector(".alert-link")).getText();
+
+		Assert.assertEquals(loginsucceeded, "Login Succeeded");
+
+		System.out.println("Test successful");
+		Thread.sleep(10000);
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+		driver.quit();
+	}
+
 }
